aa4943fb9d1439b89113647c06827a676707581c
fredy <frederikherzog05@gmail.com>
2025-09-26 18:52:57 +0200

    implement header

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+import { title } from "process";
+
+const NAV_ITEMS = [
+    {href: "/", title: "Dashboard"},
+    {href: "/search", title: "Search"},
+    {href: "/watchlist", title: "Watchlist"}
+]
+
+export default NAV_ITEMS
